18e8c0134a638e3961ec53ba54066168bd21e848
winterheeeeath <winterheatherica@gmail.com>
2024-09-08 20:02:19 +0700

    vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body class="h-full">
     <x-navbar></x-navbar>
